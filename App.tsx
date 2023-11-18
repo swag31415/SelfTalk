bd2948a7aad92d945a7f63e637d971d93acc3b27
swag31415 <kidswagatam18@gmail.com>
2023-11-18 16:02:16 -0500

    the mvp

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Chat, MessageType } from '@flyerhq/react-native-chat-ui'
+import React, { useState } from 'react'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [messages, setMessages] = useState<MessageType.Any[]>([])
+  const user = { id: '06c33e8b-e835-4736-80f4-63f44b66666c' }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const addMessage = (message: MessageType.Any) => {
+    setMessages([message, ...messages])
+  }
+
+  const handleSendPress = (message: MessageType.PartialText) => {
+    const textMessage: MessageType.Text = {
+      author: user,
+      createdAt: Date.now(),
+      id: uuidv4(),
+      text: message.text,
+      type: 'text',
+    }
+    addMessage(textMessage)
+  }
+
+  return (
+    <SafeAreaProvider>
+      <Chat
+        messages={messages}
+        onSendPress={handleSendPress}
+        user={user}
+      />
+    </SafeAreaProvider>
+  )
+}
