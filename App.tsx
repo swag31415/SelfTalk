e575ed1778c231cf6e7a9a2bdc30e18c5d49b422
swag31415 <kidswagatam18@gmail.com>
2023-11-18 17:35:13 -0500

    It works!!!!!!!

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import { Chat, MessageType, darkTheme } from '@flyerhq/react-native-chat-ui'
+import { Chat, MessageType, Theme, darkTheme, defaultTheme } from '@flyerhq/react-native-chat-ui'
 import React, { useState } from 'react'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { View, TouchableWithoutFeedback } from 'react-native'
+import { SafeAreaProvider} from 'react-native-safe-area-context'
 
 import { launchImageLibrary } from 'react-native-image-picker'
 
@@ -8,8 +9,12 @@ import 'react-native-get-random-values';
 import { v4 as uuidv4 } from 'uuid';
 
 export default function App() {
+  const users = [
+    { id: 'ae6c485e-87ea-4fca-a889-e2af0c043d46', theme: darkTheme },
+    { id: '810bb732-9382-4b43-99fb-ea642c843cc3', theme: defaultTheme }
+  ]
   const [messages, setMessages] = useState<MessageType.Any[]>([])
-  const user = { id: '06c33e8b-e835-4736-80f4-63f44b66666c' }
+  const [userIdx, setUserIdx] = useState<number>(0)
 
   const addMessage = (message: MessageType.Any) => {
     setMessages([message, ...messages])
@@ -17,7 +22,7 @@ export default function App() {
 
   const handleSendPress = (message: MessageType.PartialText) => {
     const textMessage: MessageType.Text = {
-      author: user,
+      author: users[userIdx],
       createdAt: Date.now(),
       id: uuidv4(),
       text: message.text,
@@ -39,7 +44,7 @@ export default function App() {
 
         if (response?.base64) {
           const imageMessage: MessageType.Image = {
-            author: user,
+            author: users[userIdx],
             createdAt: Date.now(),
             height: response.height,
             id: uuidv4(),
@@ -55,15 +60,34 @@ export default function App() {
     )
   }
 
+  const handleDoubleTap = () => {
+    // Switch between users
+    setUserIdx((userIdx + 1) % users.length);
+  };
+
+  let lastTap = 0;
+  const handleTouchablePress = () => {
+    const now = Date.now();
+    if (now - lastTap < 300) {
+      // Double tap detected
+      handleDoubleTap();
+    }
+    lastTap = now;
+  };
+
   return (
     <SafeAreaProvider>
-      <Chat
-        messages={messages}
-        onSendPress={handleSendPress}
-        onAttachmentPress={handleImageSelection}
-        user={user}
-        theme={darkTheme}
-      />
+      <TouchableWithoutFeedback onPress={handleTouchablePress}>
+        <View style={{ flex: 1 }}>
+          <Chat
+            messages={messages}
+            onSendPress={handleSendPress}
+            onAttachmentPress={handleImageSelection}
+            user={users[userIdx]}
+            theme={users[userIdx].theme}
+          />
+        </View>
+      </TouchableWithoutFeedback>
     </SafeAreaProvider>
-  )
+  );
 }
