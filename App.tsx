bdf2ba09f95cc8aac219410d4e16f609dfb0fb99
swag31415 <kidswagatam18@gmail.com>
2023-11-18 16:09:35 -0500

    file selection works

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import { Chat, MessageType } from '@flyerhq/react-native-chat-ui'
 import React, { useState } from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
+import DocumentPicker from 'react-native-document-picker'
+
 import 'react-native-get-random-values';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -24,11 +26,31 @@ export default function App() {
     addMessage(textMessage)
   }
 
+  const handleFileSelection = async () => {
+    try {
+      const response = await DocumentPicker.pickSingle({
+        type: [DocumentPicker.types.allFiles],
+      })
+      const fileMessage: MessageType.File = {
+        author: user,
+        createdAt: Date.now(),
+        id: uuidv4(),
+        mimeType: response.type ?? undefined,
+        name: response.name ?? '',
+        size: response.size ?? 0,
+        type: 'file',
+        uri: response.uri,
+      }
+      addMessage(fileMessage)
+    } catch {}
+  }
+
   return (
     <SafeAreaProvider>
       <Chat
         messages={messages}
         onSendPress={handleSendPress}
+        onAttachmentPress={handleFileSelection}
         user={user}
       />
     </SafeAreaProvider>
